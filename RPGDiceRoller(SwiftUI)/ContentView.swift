37479fab47cd8dd877febb6f272a23ec14bb6621
Ian Kincaid <i.s.kincaid@gmail.com>
2024-05-01 13:03:17 -0400

    Initial Commit

---
+++ RPGDiceRoller(SwiftUI)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RPGDiceRoller(SwiftUI)
+//
+//  Created by Ian Kincaid on 5/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RPGDiceRoller(SwiftUI)/RPGDiceRoller_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  RPGDiceRoller_SwiftUI_App.swift
+//  RPGDiceRoller(SwiftUI)
+//
+//  Created by Ian Kincaid on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct RPGDiceRoller_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
